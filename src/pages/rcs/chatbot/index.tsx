99a17b84c00f5dd3022ef5583e40dca3bf10752f
Shijuntao-ux <18438621787@163.com>
2024-04-12 09:30:31 +0800

    fead:chatbot列表

--- src/pages/rcs/chatbot/index.tsx
+++ src/pages/rcs/chatbot/index.tsx
@@ -10,61 +10,80 @@ import {
   Col,
   Table,
   Image,
-  Grid,
 } from 'antd'
 import type { ColumnsType } from 'antd/es/table'
 import { NavLink, useNavigate } from 'react-router-dom'
 import PageContent from '@/components/pageContent'
 import { PlusOutlined } from '@ant-design/icons'
 import codeImg from '@/assets/rcs/code.png'
+import { usePoint } from '@/hooks'
+import { getChatbot } from '@/api'
 
 import { API } from 'apis'
 import './index.scss'
 
-interface DataType extends API.GetChatbotListItem {}
+interface DataType extends API.ChatbotItem {}
+
+export enum ChatbotStatus {
+  '未提交',
+  '我方通过',
+  '驳回',
+  '审核中',
+}
+export enum ChatbotColor {
+  'color-gray',
+  'color-default',
+  'color-error',
+  'color-warning',
+}
 
 export default function Fn() {
-  const points = Grid.useBreakpoint()
+  const point = usePoint('lg')
   const nav = useNavigate()
   const [form] = Form.useForm()
-  const [tableData, settableData] = useState([
-    { name: '1213' },
-    { name: '123' },
-  ])
+  const [limit, setLimit] = useState(20)
+  const [page, setPage] = useState(1)
+  const [total, setTotal] = useState(0)
+  const [tableData, settableData] = useState<DataType[]>([])
+  const [loading, setLoading] = useState(false)
 
   const columns: ColumnsType<DataType> = [
     {
       title: '序号',
       className: 'paddingL30',
       width: 80,
-      render: (_, record) => <span>1</span>,
+      dataIndex: 'index',
     },
     {
       title: 'Chatbot名称',
       className: 'paddingL30',
       width: 150,
       fixed: true,
-      render: (_, record) => <span>技术部门</span>,
+      dataIndex: 'name',
     },
     {
       title: 'Chatbot ID',
       width: 120,
-      render: (_, record) => <span>010294912841</span>,
+      dataIndex: 'chatbotID',
     },
     {
       title: '归属CSP',
       width: 220,
-      render: (_, record) => <span>上海赛邮云技术有限公司</span>,
+      dataIndex: 'provider',
     },
     {
       title: '状态',
       width: 120,
-      render: (_, record) => <span>审核完成</span>,
+      render: (_, record) => (
+        <span className={`${ChatbotColor[record.status]}`}>
+          {ChatbotStatus[record.status]}
+        </span>
+      ),
     },
     {
       title: '更新时间',
-      width: 140,
-      render: (_, record) => <span>2024-03-10</span>,
+      width: 180,
+      dataIndex: 'datetime',
     },
     {
       title: '操作',
@@ -72,7 +91,9 @@ export default function Fn() {
       render: (_, record) => (
         <>
           <Button type='link' style={{ paddingLeft: 0 }}>
-            <NavLink to='/console/rcs/chatbot/detail'>查看</NavLink>
+            <NavLink to={`/console/rcs/chatbot/detail/${record.id}`}>
+              查看
+            </NavLink>
           </Button>
           <Button type='link' style={{ paddingLeft: 0 }}>
             <NavLink to='/console/rcs/chatbot/create/1'>编辑</NavLink>
@@ -89,6 +110,35 @@ export default function Fn() {
     nav('/console/rcs/chatbot/create/0')
   }
 
+  const getList = async () => {
+    setLoading(true)
+    try {
+      const formValues = await form.getFieldsValue()
+      const res = await getChatbot({
+        page,
+        limit,
+        ...formValues,
+      })
+      let _list: DataType[] = res.list.map((item, index) => {
+        item.index = (page - 1) * limit + index + 1
+        return item
+      })
+      settableData(_list)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+    }
+  }
+
+  const changePageInfo = (page, pageSize) => {
+    setPage(page)
+    setLimit(pageSize)
+  }
+
+  useEffect(() => {
+    getList()
+  }, [limit, page])
+
   return (
     <PageContent extClass='chatbot-list'>
       <Image src={codeImg} preview={false} width={60}></Image>
@@ -96,7 +146,7 @@ export default function Fn() {
         <div className='fn24'>申请/管理 Chatbot</div>
         <Button
           type='primary'
-          size={points.lg ? 'large' : 'middle'}
+          size={point ? 'large' : 'middle'}
           onClick={toCreate}
           icon={<PlusOutlined rev={null} className='fn14' />}>
           申请 Chatbot
@@ -109,11 +159,12 @@ export default function Fn() {
         className='chatbot-list-form'
         form={form}
         layout='vertical'
-        size={points.lg ? 'large' : 'middle'}
+        size={point ? 'large' : 'middle'}
+        initialValues={{ status: 'all', keywords: '' }}
         autoComplete='off'>
         <Row gutter={16}>
           <Col span={10} md={10} lg={8} xl={6}>
-            <Form.Item name='key' label='关键词搜索'>
+            <Form.Item name='keywords' label='关键词搜索'>
               <Input placeholder='Chatbot名称/ID' />
             </Form.Item>
           </Col>
@@ -122,16 +173,20 @@ export default function Fn() {
               <Select
                 placeholder='请选择'
                 options={[
-                  { value: 'jack', label: 'Jack' },
-                  { value: 'lucy', label: 'Lucy' },
-                  { value: 'Yiminghe', label: 'yiminghe' },
+                  { value: 'all', label: '全部' },
+                  { value: '1', label: '我方通过' },
+                  { value: '2', label: '驳回' },
+                  { value: '3', label: '审核中' },
                 ]}
               />
             </Form.Item>
           </Col>
           <Col span={6} md={4} xl={3}>
             <Form.Item label=' '>
-              <Button type='primary' className='w-100'>
+              <Button
+                type='primary'
+                className='w-100'
+                onClick={() => getList()}>
                 查询
               </Button>
             </Form.Item>
@@ -145,7 +200,17 @@ export default function Fn() {
         dataSource={tableData}
         rowKey={'name'}
         sticky
-        pagination={{ position: ['bottomRight'] }}
+        loading={loading}
+        pagination={{
+          position: ['bottomRight'],
+          current: page,
+          pageSize: limit,
+          hideOnSinglePage: true,
+          showQuickJumper: true,
+          pageSizeOptions: [10, 20, 50],
+          total: total,
+          onChange: changePageInfo,
+        }}
         scroll={{ x: 'max-content' }}
       />
     </PageContent>
