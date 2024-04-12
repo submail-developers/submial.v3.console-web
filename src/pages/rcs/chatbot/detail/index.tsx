417cf4aa3b8fa9b128956e2254274c85b9da7e3e
Shijuntao-ux <18438621787@163.com>
2024-04-12 09:29:02 +0800

    fead:chatbot详情对接

--- src/pages/rcs/chatbot/detail/index.tsx
+++ src/pages/rcs/chatbot/detail/index.tsx
@@ -9,21 +9,29 @@ import {
   Input,
   Table,
   Image,
+  ColorPicker,
 } from 'antd'
 import type { ColumnsType } from 'antd/es/table'
+import { EyeOutlined } from '@ant-design/icons'
 import PageContent from '@/components/pageContent'
 import { API } from 'apis'
 import jiqirenImg from '@/assets/rcs/jiqiren.png'
+import { useParams, NavLink, useNavigate } from 'react-router-dom'
+import { getChatbot } from '@/api'
+import APreviewImg from '@/components/aPreviewImg'
 
 import './index.scss'
 
 const { TextArea } = Input
 interface DataType extends API.GetChatbotListItem {}
 export default function Fn() {
+  const { id } = useParams()
   const [tableData, settableData] = useState([
     { name: '1223' },
     { name: '123' },
   ])
+  const [detail, setDetail] = useState<API.ChatbotItem>()
+  const [visible, setVisible] = useState(false)
 
   const columns: ColumnsType<DataType> = [
     {
@@ -60,6 +68,26 @@ export default function Fn() {
     },
   ]
 
+  const getDetail = async () => {
+    try {
+      const res = await getChatbot({
+        page: 1,
+        limit: 20,
+        appid: id,
+        status: 'all',
+      })
+      if (res.list.length == 1) {
+        setDetail(res.list[0])
+      }
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    if (id) {
+      getDetail()
+    }
+  }, [id])
+
   return (
     <PageContent extClass='chatbot-detail'>
       <Image src={jiqirenImg} preview={false} width={48}></Image>
@@ -73,8 +101,8 @@ export default function Fn() {
       <Divider className='line'></Divider>
       <div className='base-info'>
         <Space className='info-header fx-y-center' size={16}>
-          <span className='icon iconfont icon-jiqiren1 jiqiren'></span>
-          <div className='fn18 fw-500'>上海赛邮云技术部</div>
+          <span className='icon iconfont icon-jiqiren-filled jiqiren'></span>
+          <div className='fn18 fw-500'>{detail && detail.name}</div>
           <div className='status'>申请成功</div>
         </Space>
         <div className='info-content'>
@@ -83,43 +111,45 @@ export default function Fn() {
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>服务代码</div>
-                <div className='info-value'>1321321312312312</div>
+                <div className='info-value'>serviceCode-服务代码？？</div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>Chatbot ID</div>
-                <div className='info-value'>1321321312312312</div>
+                <div className='info-value'>{detail && detail.chatbotID}</div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>签名</div>
-                <div className='info-value'>上海赛邮云</div>
+                <div className='info-value'>{detail && detail.autograph}</div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>行业类型</div>
-                <div className='info-value'>上海赛邮云技术部</div>
+                <div className='info-value'>
+                  {detail && detail.actualIssueIndustry}
+                </div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>关联的CSP</div>
-                <div className='info-value'>上海赛邮云技术部</div>
+                <div className='info-value'>cspToken-关联的CSP？？？</div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>实际下发的CSP</div>
-                <div className='info-value'>上海赛邮云技术部</div>
+                <div className='info-value'>cspId-实际下发的CSP？？？</div>
               </div>
             </Col>
             <Col span={24}>
               <div className='info-item'>
                 <div className='info-label'>证明材料</div>
-                <div className='info-value'>材料</div>
+                <div className='info-value'>{detail && detail.attachment}</div>
               </div>
             </Col>
             <Col span={24}>
@@ -131,7 +161,7 @@ export default function Fn() {
                 <div className='info-value'>
                   <TextArea
                     disabled
-                    value={'0.0.0.0'}
+                    value={detail && detail.debugWhiteAddress}
                     style={{ color: '#333' }}
                     autoSize={{ minRows: 3, maxRows: 5 }}
                   />
@@ -148,55 +178,83 @@ export default function Fn() {
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>服务方电话</div>
-                <div className='info-value'>13163434353</div>
+                <div className='info-value'>{detail && detail.callback}</div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>服务方官网</div>
-                <div className='info-value'>1321321312312312</div>
+                <div className='info-value'>{detail && detail.website}</div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>服务条款链接</div>
-                <div className='info-value'>上海赛邮云</div>
+                <div className='info-value'>{detail && detail.tcPage}</div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>服务方地址</div>
-                <div className='info-value'>上海赛邮云技术部</div>
+                <div className='info-value'>{detail && detail.address}</div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>经纬度</div>
-                <div className='info-value'>上海赛邮云技术部</div>
+                <div className='info-value'>
+                  {detail && (
+                    <Space>
+                      <span>经度：lon-???</span>
+                      <span>纬度：lat-???</span>
+                    </Space>
+                  )}
+                </div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
-                <div className='info-label'>主题颜色</div>
-                <div className='info-value'>上海赛邮云技术部</div>
+                <div className='info-label'>气泡颜色</div>
+                <div className='info-value'>
+                  {detail && (
+                    <ColorPicker
+                      value={detail.colour}
+                      disabled
+                      showText={(color) => <span>{color.toHexString()}</span>}
+                    />
+                  )}
+                </div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>背景图</div>
-                <div className='info-value'>上海赛邮云技术部</div>
+                <div className='info-value'>
+                  {detail && (
+                    <Space>
+                      <Image
+                        src={detail.backgroundImage}
+                        width={32}
+                        height={32}
+                        alt=''
+                        preview={false}
+                      />
+                      <APreviewImg src={detail.backgroundImage} />
+                    </Space>
+                  )}
+                </div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>短信端口号</div>
-                <div className='info-value'>上海赛邮云技术部</div>
+                <div className='info-value'>{detail && detail.name}</div>
               </div>
             </Col>
             <Col span={24} md={12} xl={8}>
               <div className='info-item'>
                 <div className='info-label'>Chatbot提供者</div>
-                <div className='info-value'>不显示</div>
+                <div className='info-value'>{detail && detail.name}</div>
               </div>
             </Col>
             <Col span={24}>
@@ -205,7 +263,7 @@ export default function Fn() {
                 <div className='info-value'>
                   <TextArea
                     disabled
-                    value={'服务描述'}
+                    value={detail && detail.description}
                     style={{ color: '#333' }}
                     autoSize={{ minRows: 3, maxRows: 5 }}
                   />
