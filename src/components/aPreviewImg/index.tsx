f845f3134740d188c58edc3c22574dfa3ca49614
Shijuntao-ux <18438621787@163.com>
2024-04-12 09:31:02 +0800

    fead:图片预览按钮组件

---
+++ src/components/aPreviewImg/index.tsx
@@ -0,0 +1,31 @@
+import { useState, CSSProperties } from 'react'
+import { EyeOutlined } from '@ant-design/icons'
+import { Image } from 'antd'
+import './index.scss'
+
+type Props = {
+  src: string
+  style?: CSSProperties
+}
+
+// 预览图片
+export default function APreviewImg(props: Props) {
+  const [visible, setVisible] = useState(false)
+  return (
+    <div
+      className='a-preview-img-btn fx-center-center'
+      title='预览'
+      style={props.style}
+      onClick={() => setVisible(true)}>
+      <Image
+        src={props.src}
+        style={{ display: 'none' }}
+        preview={{
+          visible: visible,
+          onVisibleChange: (visible) => setVisible(visible),
+        }}
+      />
+      <EyeOutlined rev={null} />
+    </div>
+  )
+}
