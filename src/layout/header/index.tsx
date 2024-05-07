ef6c9916c88215bba7e5aea9f4fd37149ec95d2e
Shijuntao-ux <18438621787@163.com>
2024-05-07 15:17:16 +0800

    刷新素材

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -115,7 +115,7 @@ export default function MyHeader(props: Props) {
           align='center'
           style={{ flexGrow: '1', padding: '0 16px 0 0', height: '100%' }}>
           {points.sm && <MyBreadcrumb></MyBreadcrumb>}
-          <Button onClick={loginEvent}>登陆</Button>
+          {/* <Button onClick={loginEvent}>登陆</Button> */}
           {!props.hideRight && (
             <Space align='center' size={points.lg ? 16 : 0}>
               <Dropdown
