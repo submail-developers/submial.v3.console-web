55cd1fc865d388d19ffb667ce34c401c75ae2c5d
Shijuntao-ux <18438621787@163.com>
2024-05-20 14:58:07 +0800

    发送任务-主地址簿导入

--- src/pages/rcs/send/components/address/index.tsx
+++ src/pages/rcs/send/components/address/index.tsx
@@ -1,16 +1,11 @@
-import {
-  useEffect,
-  useState,
-  forwardRef,
-  useImperativeHandle,
-  useRef,
-} from 'react'
+import { forwardRef, useImperativeHandle, useRef } from 'react'
 import { API } from 'apis'
-import { getSendAddress } from '@/api'
 import AddressCollapse from './addressCollapse'
 
 // 地址簿导入
-type Props = {}
+type Props = {
+  books: API.AddressbooksItem[][]
+}
 const Fn = (props: Props, ref: any) => {
   useImperativeHandle(ref, () => {
     return {
@@ -18,31 +13,13 @@ const Fn = (props: Props, ref: any) => {
     }
   })
   const addref = useRef(null)
-  const [books, setBooks] = useState<API.AddressbooksItem[][]>([])
   const getValues = () => {
     return addref.current.getValues()
   }
 
-  const getAddress = async () => {
-    try {
-      const res = await getSendAddress({
-        page: 1,
-        type: 1,
-      })
-      if (Array.isArray(res.addressbooks)) {
-        setBooks(res.addressbooks)
-      } else {
-        setBooks(Object.values(res.addressbooks))
-      }
-    } catch (error) {}
-  }
-
-  useEffect(() => {
-    getAddress()
-  }, [])
   return (
     <div className='p-x-24 p-b-24 p-t-16 contacts-content'>
-      <AddressCollapse books={books} ref={addref} />
+      <AddressCollapse books={props.books} ref={addref} />
     </div>
   )
 }
