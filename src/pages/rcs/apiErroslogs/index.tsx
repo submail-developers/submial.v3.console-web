6ed72bcea343d739b36f66eb72784f4ca0ade857
Shijuntao-ux <18438621787@163.com>
2024-06-24 15:47:28 +0800

    API错误日志优化

--- src/pages/rcs/apiErroslogs/index.tsx
+++ src/pages/rcs/apiErroslogs/index.tsx
@@ -1,62 +1,52 @@
 import { useState, useEffect } from 'react'
-import {
-  Flex,
-  Table,
-  Row,
-  Col,
-  Button,
-  Divider,
-  Image,
-  DatePicker,
-  Form,
-  Select,
-} from 'antd'
+import { Flex, Table, Divider, Image, DatePicker, Form, Select } from 'antd'
+import type { GetProps } from 'antd'
 import PageContent from '@/components/pageContent'
 
-import { getErrorsLogs, getChatbot } from '@/api'
 import { API } from 'apis'
+import { getErrorsLogs, getChatbot } from '@/api'
 import topIco from '@/assets/rcs/errorlogs/err_ico.png'
-import './index.scss'
-import { useSize } from '@/hooks'
+import { useSize, usePoint } from '@/hooks'
+import { getPresets } from '@/utils/day'
 import dayjs from 'dayjs'
-import type { Dayjs } from 'dayjs'
-const { Option } = Select
+
+import './index.scss'
+
+type RangePickerProps = GetProps<typeof DatePicker.RangePicker>
 
 const { RangePicker } = DatePicker
-const allChatBot = {
-  id: 'all',
-  name: '全部Chatbot',
-} as API.ChatbotItem
+const rangePresets = getPresets([7, 15, 30, 90])
+// 只允许选择15天前-今天的日期
+const disabledDate: RangePickerProps['disabledDate'] = (current) => {
+  const today = dayjs()
+  const fifteenDaysAgo = today.subtract(90, 'day')
+  const currentDate = dayjs(current)
+  return currentDate.isBefore(fifteenDaysAgo) || currentDate.isAfter(today)
+}
 
 export default function Fn() {
+  const pointXs = usePoint('xs')
   const size = useSize()
   const [form] = Form.useForm()
   const [loading, setLoading] = useState(false)
-  const [currentPage, setcurrentPage] = useState<number>(1)
-  const [pageSize, setpageSize] = useState<number>(40)
+  const [page, setPage] = useState<number>(1)
+  const [limit, setLimit] = useState<number>(10)
   const [total, setTotal] = useState<number>(0)
   const [getErrorList, setGetErrorList] = useState<API.GetErrorsLogsItems[]>()
-  const [chatBotList, setChatBotList] = useState<API.ChatbotItem[]>([
-    allChatBot,
-  ])
+  const [chatBotList, setChatBotList] = useState<API.ChatbotItem[]>([])
 
   // 获取chatbot
   const getChatbotList = async () => {
-    setLoading(true)
     try {
       const res = await getChatbot({
-        page: currentPage,
-        limit: pageSize,
+        page: 1,
+        limit: 1000,
         appid: '',
         keywords: '',
         status: 'all',
       })
-      setChatBotList([...chatBotList, ...res.list])
-
-      setLoading(false)
-    } catch (error) {
-      setLoading(false)
-    }
+      setChatBotList(res.list)
+    } catch (error) {}
   }
 
   // 获取错误日志
@@ -64,18 +54,15 @@ export default function Fn() {
     setLoading(true)
     try {
       const formValues = await form.getFieldsValue()
-      const start =
-        (formValues.time && formValues.time[0].format('YYYY-MM-DD')) || ''
-      const end =
-        (formValues.time && formValues.time[1].format('YYYY-MM-DD')) || ''
+      const start = formValues.time[0].format('YYYY-MM-DD')
+      const end = formValues.time[1].format('YYYY-MM-DD')
       let params = {
         appid: formValues.chatbot,
         start,
         end,
-        page: currentPage,
-        limit: pageSize,
+        page: page,
+        limit: limit,
       }
-
       const res = await getErrorsLogs(params)
       setGetErrorList(res.data)
       setTotal(res.rows)
@@ -85,27 +72,39 @@ export default function Fn() {
     }
   }
 
-  const onRangeChange = (dates, dateStrings) => {
-    if (dates) {
-      console.log('From: ', dates[0], ', to: ', dates[1])
-      console.log('From: ', dateStrings[0], ', to: ', dateStrings[1])
+  const changePageInfo = (page, pageSize) => {
+    if (pageSize != limit) {
+      setPage(1)
+      setLimit(pageSize)
     } else {
-      console.log('Clear')
+      setPage(page)
     }
   }
 
-  useEffect(() => {
+  // 除搜索关键字，其他字段改变直接搜索
+  const onValuesChange = (changedValues, allValues) => {
     getList()
+  }
+
+  useEffect(() => {
     getChatbotList()
   }, [])
+
+  useEffect(() => {
+    getList()
+  }, [limit, page])
 
   const columns = [
     {
       title: 'Chatbot',
-      dataIndex: 'chatbot_name',
       fixed: true,
-      width: size == 'small' ? 150 : 240,
+      width: size == 'small' ? 150 : 200,
       className: size == 'small' ? 'paddingL20' : 'paddingL30',
+      render: (_, record) => (
+        <div style={{ height: 40 }} className='fx-y-center'>
+          {record.chatbot_name}
+        </div>
+      ),
     },
     {
       title: 'API',
@@ -129,110 +128,65 @@ export default function Fn() {
     },
     {
       title: '详情',
-      dataIndex: 'detail',
-      key: 'detail',
       render: (_, record) => (
-        <div className='g-ellipsis-2' style={{ width: '240px' }}>
+        <div
+          className='g-ellipsis-2 p-r-24'
+          style={{ width: '240px' }}
+          title={record.msg}>
           {record.msg}
         </div>
       ),
     },
   ]
 
-  const rangePresets: {
-    label: string
-    value: [Dayjs, Dayjs]
-  }[] = [
-    {
-      label: '最近 7 天',
-      value: [dayjs().add(-7, 'd'), dayjs()],
-    },
-    {
-      label: '最近 15 天',
-      value: [dayjs().add(-15, 'd'), dayjs()],
-    },
-    {
-      label: '最近一个月',
-      value: [dayjs().add(-30, 'd'), dayjs()],
-    },
-    {
-      label: '最近三个月',
-      value: [dayjs().add(-90, 'd'), dayjs()],
-    },
-  ]
-
   return (
     <PageContent extClass='api-errorlogs'>
+      <Image src={topIco} preview={false} width={72}></Image>
+      <Flex justify='space-between' align='center' className='m-t-4'>
+        <div className='fn22 fw-500'>API错误日志</div>
+      </Flex>
+      <Divider />
       <Form
         form={form}
         className='api-errorlogs-form'
         name='api-errorlogs'
         layout='vertical'
         autoComplete='off'
+        onValuesChange={onValuesChange}
         initialValues={{
-          chatbot: 'all',
-          time: [dayjs().add(-1, 'd'), dayjs().add(0, 'd')],
+          time: rangePresets[3].value,
         }}>
-        <Image src={topIco} preview={false} width={72}></Image>
-        <Flex
-          justify='space-between'
-          align='center'
-          style={{ marginTop: '4px' }}>
-          <div className='fn22 fw-500'>API错误日志</div>
+        <Flex align='flex-end' wrap='wrap' gap={16}>
+          <Form.Item label='Chatbot选择' name='chatbot' className='m-b-0'>
+            <Select
+              placeholder='全部Chatbot'
+              allowClear
+              popupMatchSelectWidth={120}
+              style={{ width: 120 }}
+              options={chatBotList}
+              fieldNames={{ label: 'name', value: 'id' }}></Select>
+          </Form.Item>
+          <Form.Item label='时间范围' name='time' className='m-b-0'>
+            <RangePicker
+              presets={!pointXs && rangePresets}
+              allowClear={false}
+              disabledDate={disabledDate}></RangePicker>
+          </Form.Item>
         </Flex>
-        <Divider className='line'></Divider>
-
-        <Row gutter={16}>
-          <Col className='top-item m-b-20'>
-            <Form.Item
-              label='Chatbot选择'
-              name='chatbot'
-              style={{ width: '140px', marginBottom: '0px' }}>
-              <Select placeholder='所有标签' popupMatchSelectWidth={120}>
-                {chatBotList?.map((option) => (
-                  <Option key={option.id} value={option.id}>
-                    {option.name}
-                  </Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col className='top-item m-b-20'>
-            <Form.Item
-              label='时间范围'
-              name='time'
-              style={{ marginBottom: '0px' }}>
-              <RangePicker
-                clearIcon={false}
-                presets={rangePresets}
-                onChange={onRangeChange}
-                style={{ width: size == 'small' ? 190 : 240 }}></RangePicker>
-            </Form.Item>
-          </Col>
-
-          <Col span={6} md={4} xl={3}>
-            <Form.Item label=' '>
-              <Button
-                type='primary'
-                className='w-100'
-                htmlType='submit'
-                onClick={() => getList()}>
-                查询
-              </Button>
-            </Form.Item>
-          </Col>
-        </Row>
-
         <Table
-          className='theme-cell reset-table'
+          loading={loading}
+          className='theme-cell reset-table m-t-24'
           columns={columns}
           dataSource={getErrorList}
           rowKey={'id'}
-          sticky
           pagination={{
             position: ['bottomRight'],
-            pageSize: 100,
-            pageSizeOptions: [100, 200, 300],
+            current: page,
+            pageSize: limit,
+            showQuickJumper: true,
+            pageSizeOptions: [10, 20, 50],
+            total: total,
+            onChange: changePageInfo,
           }}
           scroll={{ x: 'max-content' }}
         />
