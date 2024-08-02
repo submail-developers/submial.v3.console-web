9cc30f16a59d1595102a34a3a50eeed870c733dd
Shijuntao-ux <18438621787@163.com>
2024-08-02 13:32:59 +0800

    智能语音机器人-偏好配置

---
+++ src/pages/voiceChatbot/setting/components/email/index.tsx
@@ -0,0 +1,123 @@
+import { useState, useEffect, useRef } from 'react'
+import { Flex, Row, Col, Space, Switch, Popconfirm } from 'antd'
+import { settingRcs, initSetting } from '@/store/reducers/settingRcs'
+import { useAppSelector, useAppDispatch } from '@/store/hook'
+import { changeRcsSetting, delRcsSettingMobEmail } from '@/api'
+import AddEmail from '../addEmail'
+import { uniqBy } from 'lodash'
+import { API } from 'apis'
+
+import { usePoint } from '@/hooks'
+
+export default function Fn() {
+  const dispatch = useAppDispatch()
+  const pointLg = usePoint('lg')
+  const ref = useRef(null)
+  const rcsSetting = useAppSelector(settingRcs)
+  const [switchLoading, setSwitchLoading] = useState(false)
+  const [switchVal, setSwitchVal] = useState(false)
+  const [list, setList] = useState<API.SettingReminderItem[]>([])
+
+  const changeRcsSettings = async (
+    item: API.ChangeRcsSettingSwitchType,
+    value: API.SettingStatus,
+  ) => {
+    setSwitchLoading(true)
+    try {
+      const res = await changeRcsSetting({ item, value })
+      if (res.status == 'success') {
+        setSwitchVal(value == '1')
+        dispatch(initSetting())
+      }
+      setSwitchLoading(false)
+    } catch (error) {
+      setSwitchLoading(false)
+    }
+  }
+  const delEvent = async (id: string) => {
+    try {
+      const res = await delRcsSettingMobEmail({
+        id,
+      })
+      if (res.status == 'success') {
+        dispatch(initSetting())
+      }
+    } catch (error) {}
+  }
+  useEffect(() => {
+    if (rcsSetting) {
+      let default_list: API.SettingReminderItem[] = [
+        {
+          id: '-1',
+          type: '0',
+          address: rcsSetting.settings.account,
+          status: '0',
+        },
+      ]
+      const _list = rcsSetting.settings.reminder_list.filter(
+        (item) => item.type == '0',
+      )
+      setList(uniqBy([...default_list, ..._list], 'address'))
+      setSwitchVal(rcsSetting.settings.reminder_mail == '1')
+    }
+  }, [rcsSetting])
+  return (
+    <>
+      <Row>
+        <Col span={12} lg={6} xl={4}>
+          <span className='fw-500 fn16'>邮件提示</span>
+        </Col>
+        <Col span={12} lg={18} xl={20}>
+          <Flex justify={pointLg ? 'flex-start' : 'flex-end'}>
+            <Switch
+              size='small'
+              checked={switchVal}
+              onChange={(flag) =>
+                changeRcsSettings('reminder_mail', flag ? '1' : '0')
+              }
+              loading={switchLoading}></Switch>
+          </Flex>
+        </Col>
+        <Col span={24} lg={6} xl={4}></Col>
+        <Col span={24} lg={18} xl={20}>
+          <Row className='gray-color m-t-12' gutter={[16, 12]}>
+            {list.map((item) => (
+              <Col span={24} key={item.id}>
+                {rcsSetting?.settings.account == item.address ? (
+                  <Space size={24}>
+                    <span className='gray-color-sub'>
+                      {item.address}（账户邮箱不可更改）
+                    </span>
+                  </Space>
+                ) : (
+                  <Space size={24}>
+                    <span>{item.address}</span>
+                    <Popconfirm
+                      placement='bottom'
+                      title='警告'
+                      description='确定删除该邮箱吗？'
+                      onConfirm={() => delEvent(item.id)}
+                      okText='确定'
+                      cancelText='取消'>
+                      <span className='icon iconfont icon-shanchu color g-pointer fn14'></span>
+                    </Popconfirm>
+                  </Space>
+                )}
+              </Col>
+            ))}
+            <Col span={24}>
+              <Space
+                align='center'
+                className='color g-pointer'
+                onClick={() => ref.current.open()}>
+                <span className='icon iconfont icon-jia fn13'></span>
+                <span>新增</span>
+              </Space>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <AddEmail ref={ref} />
+    </>
+  )
+}
---
+++ src/pages/voiceChatbot/setting/index.tsx
@@ -0,0 +1,50 @@
+import { Flex, Row, Col, Divider, Image } from 'antd'
+import PageContent from '@/components/pageContent'
+import Credits from './components/credits'
+import Mob from './components/mob'
+import Mail from './components/email'
+import Safe from './components/safe'
+
+import faceImg from '@/assets/rcs/face/settings.png'
+
+import './index.scss'
+
+export default function Fn() {
+  return (
+    <PageContent extClass='setting'>
+      <Image src={faceImg} preview={false} width={72}></Image>
+      <Flex justify='space-between' align='center' className='m-t-4'>
+        <div className='fn22 fw-500'>
+          偏好设置
+          <div
+            className='fn14 gray-color m-t-10'
+            style={{ fontWeight: 'normal' }}>
+            配置您的5G消息账户，更改或更新余额共享设置、余额提醒、提醒列表、安全设置以及其他设置。
+          </div>
+        </div>
+      </Flex>
+      <Divider />
+      <Row gutter={[24, 24]}>
+        <Col span={24}>
+          {/* 余额 */}
+          <Credits />
+        </Col>
+        <Col span={24}>
+          {/* 邮箱 */}
+          <Mail />
+        </Col>
+        <Col span={24}>
+          {/* 手机号 */}
+          <Mob />
+        </Col>
+        <Col span={24}>
+          <Divider />
+        </Col>
+        <Col span={24}>
+          {/* 安全设置 */}
+          <Safe />
+        </Col>
+      </Row>
+    </PageContent>
+  )
+}
